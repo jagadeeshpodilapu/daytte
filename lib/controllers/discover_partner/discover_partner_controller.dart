25bb8985933b65a66a37c074fd3b52f1320497d9
raji <rajyamelec99@gmail.com>
2021-12-05 18:34:10 +0530

    just push

--- lib/controllers/discover_partner/discover_partner_controller.dart
+++ lib/controllers/discover_partner/discover_partner_controller.dart
@@ -63,6 +63,8 @@ class DiscoverPartnerController extends BaseController {
       print('galleryImages and length $response $userId ${gallery.value}');
     }
     update();
+
+
     listUserGalleryCalling();
   }
 
