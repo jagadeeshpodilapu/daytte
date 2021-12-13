95f1434728866ddc40e28edf8a36d93b8d8eb9ce
raji <rajyamelec99@gmail.com>
2021-12-13 22:43:17 +0530

    profile full details completed

--- lib/view/screens/discoverpartner/expand_discover_partner.dart
+++ lib/view/screens/discoverpartner/expand_discover_partner.dart
@@ -1,10 +1,8 @@
 import 'dart:ui';
-
 import 'package:daytte/consts/constants.dart';
 import 'package:daytte/themes/app_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../consts/image_constants.dart';
 import '../../../model/find_nearest_model.dart';
 import '../../widgets/common_widgets.dart';
@@ -17,60 +15,55 @@ class ExpandPartnerDetails extends StatelessWidget {
     return Hero(
       tag: "img",
       child: Scaffold(
-        body: Container(
-          child: Stack(
-            fit: StackFit.expand,
-            clipBehavior: Clip.none,
-            children: [
-              _userProfilePicWidget(),
-              _downArrowWidget(),
-              _userBioDetailsWidget(),
-            ],
-          ),
+        body: CustomScrollView(
+          shrinkWrap: true,
+          slivers: <Widget>[
+            SliverAppBar(
+             backgroundColor: Colors.white12,
+              expandedHeight: 250.0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network('${user.profileImg?.imgPath}',
+                    fit: BoxFit.cover),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                addVerticalSpace(10),
+                _userNameWidget(),
+                addVerticalSpace(10),
+                _infoWidget(Icons.business_center,
+                    "${user.institute ?? "Wildlife Photographer"}"),
+                addVerticalSpace(5),
+                _infoWidget(Icons.location_city_rounded, "Lives in New York"),
+                addVerticalSpace(5),
+                _infoWidget(Icons.person, "${user.gender}"),
+                addVerticalSpace(5),
+                _infoWidget(Icons.location_on, "2Km away"),
+                _aboutmeText("About Me"),
+                _aboutmeInfoWidget(),
+                addVerticalSpace(20),
+                _divider,
+                addVerticalSpace(20),
+                _aboutmeText("Passions"),
+                _passionListWidget(),
+                addVerticalSpace(20),
+                _divider,
+                addVerticalSpace(20),
+                _shareProfileWidget(),
+                _shareWidgetSub(),
+                addVerticalSpace(20),
+                _divider,
+                addVerticalSpace(20),
+                _reportProfileWidget(),
+                addVerticalSpace(20),
+                _divider,
+                addVerticalSpace(40),
+                _setIconsWidget(),
+                addVerticalSpace(30),
+              ]),
+            ),
+          ],
         ),
-      ),
-    );
-  }
-
-  Widget _userBioDetailsWidget() {
-    return FractionallySizedBox(
-      heightFactor: 0.5,
-      alignment: Alignment.bottomCenter,
-      child: ListView(
-        children: [
-          addVerticalSpace(10),
-          _userNameWidget(),
-          addVerticalSpace(10),
-          _infoWidget(Icons.business_center,
-              "${user.institute ?? "Wildlife Photographer"}"),
-          addVerticalSpace(5),
-          _infoWidget(Icons.location_city_rounded, "Lives in New York"),
-          addVerticalSpace(5),
-          _infoWidget(Icons.person, "${user.gender}"),
-          addVerticalSpace(5),
-          _infoWidget(Icons.location_on, "2Km away"),
-          _aboutmeText("About Me"),
-          _aboutmeInfoWidget(),
-          addVerticalSpace(20),
-          _divider,
-          addVerticalSpace(20),
-          _aboutmeText("Passions"),
-          _passionListWidget(),
-          addVerticalSpace(20),
-          _divider,
-          addVerticalSpace(20),
-          _shareProfileWidget(),
-          _shareWidgetSub(),
-          addVerticalSpace(20),
-          _divider,
-          addVerticalSpace(20),
-          _reportProfileWidget(),
-          addVerticalSpace(20),
-          _divider,
-          addVerticalSpace(40),
-          _setIconsWidget(),
-          addVerticalSpace(30),
-        ],
       ),
     );
   }
